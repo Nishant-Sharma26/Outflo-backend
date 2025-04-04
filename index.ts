726237e94f6ab109a5690c85464878e0d42fcbd9
Nishant Sharma <nishusharma1817@gmail.com>
2025-04-05 05:27:14 +0530

    port update for backend

--- index.ts
+++ index.ts
@@ -18,4 +18,8 @@ connectDB();
 app.use("/campaigns", campaignRoutes);
 app.use("/", messageRoutes);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
