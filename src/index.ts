50a34af3d721325dcd541e917849552502a8c20c
Nishant Sharma <nishusharma1817@gmail.com>
2025-04-05 11:14:42 +0530

    update port

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,34 @@
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./db";
-import router from "./routes/messages";
+import messagesRouter from "./routes/messages"; 
+import campaignsRouter from "./routes/campaigns"; 
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "*" })); 
-app.use(router);
+app.use(cors({ origin: "*" }));
 
-const PORT = process.env.PORT || 3000;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.use("/messages", messagesRouter); 
+app.use("/campaigns", campaignsRouter);
+
+
+
+
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  connectDB().then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  }).catch((error) => {
+    console.error("Failed to connect to MongoDB:", error);
   });
-});
-
+}
 export default app;
 
